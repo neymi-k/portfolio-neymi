934d1952e2653dbf3fefc348a47b19333ddc2760
neymi-k <148432516+neymi-k@users.noreply.github.com>
2024-02-05 12:50:28 +0100

    add folder structure and navbar component

---
+++ src/Componens/Navbar/index.tsx
@@ -0,0 +1,10 @@
+
+
+const Navbar = () => {
+    return (
+     <div>
+        <h1 className=' text-black/50'>Hola soy Neymi</h1>
+     </div>
+)}
+
+export default Navbar;
---
+++ src/Pages/App/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../../Componens/Navbar'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
